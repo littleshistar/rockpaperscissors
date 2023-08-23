4779fe261bff487e16f2567342c531c6a5cc5677
littleshistar <116385291+littleshistar@users.noreply.github.com>
2023-08-23 16:23:44 -0300

    Add playRound function

---
+++ script.js
@@ -0,0 +1,19 @@
+// Random choice function
+// Needs to choose between rock, paper or scissors
+// Once chosen, send result to console to compare with player's
+// Make win/lose text depending on comparison results
+
+function playRound(playerChoice, computerChoice) {
+            //Player wins
+    if ((playerChoice === "Scissors" && computerChoice === "Paper") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Rock" && computerChoice === "Scissors")) {
+        return console.log(`You Win! ${playerChoice} beats ${computerChoice}.`)
+    } 
+            // Player loses
+    else if ((playerChoice === "Scissors" && computerChoice === "Rock") || (playerChoice === "Paper" && computerChoice === "Scissors") || (playerChoice === "Rock" && computerChoice === "Paper")) {
+    return console.log(`You Lose! ${computerChoice} beats ${playerChoice}.`)
+    }
+            // Player inputs something that isn't rock, paper or scissors
+    else  if (playerChoice !== "Paper"||"Rock"||"Scissors") {
+        return console.log("It seems there was a typo in your input. Please try again.")
+    }
+}
