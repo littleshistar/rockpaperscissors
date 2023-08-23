da4624b40525e0f18f8d6939d8f8845ca9c6525b
littleshistar <116385291+littleshistar@users.noreply.github.com>
2023-08-23 17:09:31 -0300

    Add getPlayerChoice and draw result in playRound

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Random choice function
-// Needs to choose between rock, paper or scissors
-// Once chosen, send result to console to compare with player's
-// Make win/lose text depending on comparison results
+            //Define Player's choice
+
+let playerChoice;
+
+function getPlayerChoice() {
+    let userInput = prompt("Welcome to Rock, Paper, Scissors! Please write your choice here:")
+    let userInputClean = userInput.toLowerCase()
+    playerChoice = userInputClean.charAt(0).toUpperCase() + userInputClean.slice(1);
+}
+
+            //Define Computer's choice
 
 const choices = ["Rock", "Paper", "Scissors"];
 let computerChoice;
@@ -10,14 +17,22 @@ function getComputerChoice() {
     computerChoice = (choices[(Math.floor(Math.random() * choices.length))]);
 }
 
+            //Define who wins
+
 function playRound(playerChoice, computerChoice) {
+    getPlayerChoice();
+    getComputerChoice();
             //Player wins
     if ((playerChoice === "Scissors" && computerChoice === "Paper") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Rock" && computerChoice === "Scissors")) {
         return console.log(`You Win! ${playerChoice} beats ${computerChoice}.`)
     } 
             // Player loses
     else if ((playerChoice === "Scissors" && computerChoice === "Rock") || (playerChoice === "Paper" && computerChoice === "Scissors") || (playerChoice === "Rock" && computerChoice === "Paper")) {
-    return console.log(`You Lose! ${computerChoice} beats ${playerChoice}.`)
+        return console.log(`You Lose! ${computerChoice} beats ${playerChoice}.`)
+    }
+            // Draw
+    else if ((playerChoice === "Scissors" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Paper") || (playerChoice === "Rock" && computerChoice === "Rock")) {
+        return console.log(`It's a Draw!`)
     }
             // Player inputs something that isn't rock, paper or scissors
     else  if (playerChoice !== "Paper"||"Rock"||"Scissors") {
