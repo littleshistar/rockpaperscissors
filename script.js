e608d635d27a3786ac948f4897db1eb6cd9b7796
littleshistar <116385291+littleshistar@users.noreply.github.com>
2023-09-11 11:50:10 -0300

    Add basic HTML, button functionality & results

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-            //Define Player's choice
-
-function getPlayerChoice() {
-    let userInput = prompt("Welcome to Rock, Paper, Scissors! Please write your choice here:")
-    let playerChoice = userInput.trim().toLowerCase()
-    return playerChoice;
-}
-
-            //Define Computer's choice
+//Define Computer's choice
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -14,36 +6,41 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-            // Capitalize choices function
+// Capitalize choice function
             
 function capitalize(choice) {
     let ChoiceCapital = choice.charAt(0).toUpperCase() + choice.slice(1);
     return ChoiceCapital; 
 }
 
-            //Define who wins
+//Define who wins
 
-function playRound() {
-    let playerChoice = getPlayerChoice();
+function playRound(playerChoice) {
     let computerChoice = getComputerChoice();
 
     let playerChoiceCapital = capitalize(playerChoice);
     let computerChoiceCapital = capitalize(computerChoice);
 
+    const result = document.querySelector("#result"); 
+
             //Player wins
     if ((playerChoice === "scissors" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "rock" && computerChoice === "scissors")) {
-        return console.log(`You Win! ${playerChoiceCapital} beats ${computerChoiceCapital}.`)
+        result.textContent= `You Win! ${playerChoiceCapital} beats ${computerChoiceCapital}.`;
     } 
             // Player loses
     else if ((playerChoice === "scissors" && computerChoice === "rock") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "rock" && computerChoice === "paper")) {
-        return console.log(`You Lose! ${computerChoiceCapital} beats ${playerChoiceCapital}.`)
+        result.textContent=`You Lose! ${computerChoiceCapital} beats ${playerChoiceCapital}.`;
     }
             // Draw
     else if (playerChoice === computerChoice) {
-        return console.log(`It's a Draw!`)
-    }
-            // Player inputs something that isn't rock, paper or scissors
-    else  if (playerChoice !== "paper"||"rock"||"scissors") {
-        return console.log("It seems there was a typo in your input. Please try again.")
+        result.textContent= `It's a Draw!`;
     }
 }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let choice = button.id;
+    playRound(choice);
+  });
+});
